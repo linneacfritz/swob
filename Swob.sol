2c0f59f49842f63e6b7ee6c36ad130485ff109d4
linneacfritz <linneafritz@gmail.com>
2016-11-20 21:05:30 +0100

    Add files via upload

---
+++ Swob.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.4;
+contract Swob {
+
+struct Match {
+uint hardware;
+uint software;
+uint timestamp;
+}
+
+address caller;
+address public leader;
+address public winner;
+uint public nodeId;
+Match originalMatch;
+Match bestMatch;
+
+
+
+modifier onlyCaller(){
+if (msg.sender != caller) throw;
+_;
+}
+
+function call (uint n, uint s, uint h, uint t){
+caller = msg.sender;
+nodeId=n;
+bestMatch.software=s;
+bestMatch.hardware=h;
+bestMatch.timestamp=t;
+
+originalMatch.software=s;
+originalMatch.hardware=h;
+originalMatch.timestamp=t;
+}
+
+function foundMatch (uint sw, uint hw, uint ts){
+  if (sw != bestMatch.software || hw != bestMatch.hardware){
+    if (ts > bestMatch.timestamp){
+    bestMatch.software= sw;
+    bestMatch.hardware = hw;
+    bestMatch.timestamp = ts;
+    leader = msg.sender;
+    }
+  }
+}
+
+function sameMatch returns bool {
+  if (bestMatch.software == originalMatch.software &&
+  bestMatch.hardware == originalMatch.hardare){
+    return true;
+  }
+  else return false;
+}
+
+function endCall() onlyCaller {
+winner=leader;
+//Because a contract cannot return a struct to an outside call, it can maybe
+//return the address of the winner, and perhaps get the new information to the
+//database through another medium than the blockchain.
+//caller.send(bestMatch);
+caller.send(winner);
+}
+}
