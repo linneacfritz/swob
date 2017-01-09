009922164203589c9a337eadefdde29eebe326c6
linneacfritz <linneafritz@gmail.com>
2017-01-09 12:38:36 +0100

    Add files via upload

--- swob/app/javascripts/app.js
+++ swob/app/javascripts/app.js
@@ -1,208 +1,153 @@
-//var accounts;
-var account;
-var workbook;
+var accounts = web3.eth.accounts;
+var workbook
 var sheet_name_list;
-var swob = Swob.deployed();
-  var match ={node:null, hardW:null, softW:null, timeS:null};
-//import "swob/contracts/Swob.sol";
-//var Swob = require ("/home/theuser/swob/build/contracts/Swob.sol.js");
-//var SimpleNameRegistry = require("example-truffle-library/build/contracts/SimpleNameRegistry.sol.js");
-//var Web3 = require('web3');
+var date = new Date();
+var timestamp1= 1234567;
+var timestamp2=2222222;
+var swob ;
+var match ={node:null, hardW:null, softW:null, timeS:null};
+
+var match1 = {node:123, hardW:44, softW:33, timeS:789};
+var match2 = {node:123, hardW:44, softW:33, timeS:789};
+var match3 = {node:123, hardW:784, softW:33, timeS:989};
+var match4 = {node:123, hardW:44, softW:33, timeS:789};
+var match5 = {node:123, hardW:55, softW:33, timeS:1890};
+var matchy =[match1, match2, match3, match4, match5];
 var Web3 = require("/usr/lib/node_modules/truffle/node_modules/web3-provider-engine/node_modules/web3");
 var web3 = new Web3();
-//var web3 = require('/usr/lib/node_modules/truffle/node_modules/ethereum.js');
-//var Web3 = require('/usr/lib/node_modules/truffle/node_modules/web3');
 
 
-module.exports = function(callback) {
+module.exports = function(callback) {}
 
 setProvider();
-getAddresses();
 createWorkBook();
-swob.startCall(123456);
-getNodeId();
-table1(123456);
-swob.foundMatch(match.softW, match.hardW, match.timeS);
-getLeader();
-}
+makeStaticChecks();
 
 
-function setProvider(){
-  if(!web3.currentProvider){
-    web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"))};
-  console.log(web3.currentProvider);
-}
-
-function getAddresses(){
-  web3.eth.getAccounts(function(err, accs) {
-    if (err != null) {
-      alert("There was an error fetching your accounts.");
-      return;
-    }
-
-    if (accs.length == 0) {
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-      return;
-    }
-
-    accounts = accs;
-    account = accounts[0];
-console.log("kolla här, adress nummer 5: " + accounts[4]);
-
-  });
-}
-
-
-
-function createWorkBook(){
-  if(typeof require !== 'undefined') XLSX = require('/usr/lib/node_modules/truffle/node_modules/xlsx');
-  workbook = XLSX.readFile('/home/linnea/swob/app/javascripts/out.xlsx');
-  sheet_name_list = workbook.SheetNames;
-}
-
-function table1(id){
-  var nodeId=id;
-  match.node=nodeId;
-  var worksheet = workbook.Sheets[sheet_name_list[0]];
-   //FULHACK
-  var range = worksheet['!ref'];
-  var found=false;
-  var end = range[range.length-1];
-  var hardware;
-  console.log(range);
-
-  for(i=1; i<=end; i++){
-    var row = 'A'+i;
-    var value = worksheet[row].v;
-
-    if(value == nodeId){
-      found=true;
-      hardware = worksheet['B'+i];
-      match.hardW=hardware.v;
-      console.log("hardware: " + hardware.v);
-    }
-
-    console.log(value);
+  function setProvider(){
+    if(!web3.currentProvider){
+      web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"))
+    };
   }
-    if (found){
-      console.log("the value was found");
-      table2(hardware.v);
-    }
-    else{console.log("didnt find value");
-    }
-}
 
-function table2(hw){
-  var hardware=hw;
-  var software;
-  var worksheet = workbook.Sheets[sheet_name_list[1]];
-   //FULHACK
-  var range = worksheet['!ref'];
-  var found=false;
-  var end = range[range.length-1];
-  console.log(range);
+  function makeFiveChecks(nodeId){
+    matchingAlgo(nodeId);
+    for(i=0; i<5; i++){
+      web3.eth.defaultAccount=accounts[i];
+      swob = Swob.deployed();
+      //swob.startCall(nodeId)
+      createMatch(accounts[i]);
+      getTimestamp();
 
-  for(i=1; i<=end; i++){
-    var row = 'A'+i;
-    var value = worksheet[row].v;
-    console.log("value: " + value + " hardware: " + hardware);
-    if(value == hardware){
-      found=true;
-      software = worksheet['B'+i];
-      match.softW=software.v;
-      match.timeS=123455666;
-      console.log("software: " + software.v);
     }
-
-    console.log(value);
   }
-     if (found){
-       console.log("the value was found");
-     }
-     else{console.log("didnt find value");}
-}
 
+  function makeStaticChecks(){
+    swob = Swob.deployed();
+    swob.startCall(123);
+    for(i=0; i<5; i++){
+      web3.eth.defaultAccount=accounts[i];
+      swob.createMatch(matchy[i].hardW, matchy[i].softW, matchy[i].timeS, {from: accounts[i]});
+      getTimestamp(i);
+    }
+  }
 
+  function createWorkBook(){
+    if(typeof require !== 'undefined') XLSX = require('/usr/lib/node_modules/truffle/node_modules/xlsx');
+    //workbook = XLSX.readFile('/home/linnea/swob/app/javascripts/out.xlsx');
+    workbook = XLSX.readFile('/home/theuser/swob/out.xlsx');
+    sheet_name_list = workbook.SheetNames;
+  }
 
-//var SimpleNameRegistry = require("example-truffle-library/build/contracts/SimpleNameRegistry.sol.js");
+  function startCall(nodeId) {
+    swob.startCall(nodeId).then(function(value){
+    console.log("message sender: " + value);
+    });
+  }
 
-//module.exports = function(deployer) {
-  // Deploy our contract, then set the address of the registry.
-  //deployer.deploy(MyContract).then(function() {
-    //MyContract.deployed().setRegistry(SimpleNameRegistry.address);
-  //});
-//};
+  function table(id, sheet_number){
+    var worksheet = workbook.Sheets[sheet_name_list[sheet_number]];
+    var range = worksheet['!ref'];
+    var end = range[range.length-1];
+    var answer;
+    for(i=1; i<=end; i++){
+      var row = 'A'+i;
+      var value = worksheet[row].v;
+      if(value == id){
+        return (worksheet['B'+i].v);
+      }
+    }
+    return 0;
+  }
 
-//function setStatus(message) {
-//  var status = document.getElementById("status");
-//  status.innerHTML = message;
-//};
+  function matchingAlgo(number){
+    var answer1 = table(number, 0);
+    var answer2=0;
 
-/*function printNodeId(){
-  var swob = Swob.deployed();
-  swob.getNodeId.call({from: account}).then(function(value){
-var nodeId= 87;
-    //console.log(Swob.deployed());
-console.log(nodeId);
+    if (answer1!=0){
+      match.node=number;
+      match.hardW=answer1;
+      answer2=table(answer1, 1);
+    }
 
-//    var node_elem = document.getElementById("node");
-  //  node_elem.innerHTML = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    //setStatus("Error printing node id; see log.");
-  });
-};
-});*/
-
-
-
-
-
-
-
-function getNodeId(){
-swob.getNodeId({from: account}).then(function(value){
-  console.log("Node id has been set to:" + value);
-})
+    if (answer2!=0){
+      match.softW=answer2;
+      match.timeS = date.getTime();
+    }
   };
 
-function getLeader(){
-  swob.leader({from: account}).then(function(value){
-    console.log("Leader is: " + value);
-  })
-};
+  function createMatch(a){
+    //web3.eth.defaultAccount = a;
+    //console.log("the sending account is now: " + a);
+    swob.createMatch(match.hardW, match.softW, match.timeS, {from: a}).then(function (value) {
+      console.log("the sender this time was: " + value);
+    });
+  }
 
-function getBalance(){
+  function getNodeId(){
+    swob.nodeId.call({from: account}).then(function(value){
+    console.log("Node is: " + value);
+    });
+  };
 
-  swob.getBalance.call(account, {from:account}).then(function(value){
-    var balance_element = document.getElementById("balance");
-    balance_element.innerHTML = value.valueOf();
+  function getTimestamp(j){
+    /*swob.getBestHardware.call(j).then(function(value){
+    console.log("Hardware of best match is: " + value);
+    });
 
-  }).catch(function (e){
-    console.log(e);
-    //setStatus("Error getting balance; see log.");
-  });
-};
+    swob.number_of_checks.call().then(function(value){
+    console.log("number_of_checks: " + value);
+    });
 
+    swob.caller.call().then(function(value){
+    console.log("message sender: " + value);
+    });*/
 
+    swob.getBestMatch.call().then(function(val1){
+    console.log("BESTIE! " + val1);
+    });
 
-function sendCall(){
+    /*swob.getBestSoftware.call(j).then(function(value){
+    console.log("Software of best match is: " + value);
+    });
 
+    swob.getLeader.call(j).then(function(value){
+    console.log("The leader is: " + value);
+    });
 
-  var nodeId = parseInt(document.getElementById("nodeId").value);
-  var software = parseInt(document.getElementById("software").value);
-  var hardware = parseInt(document.getElementById("hardware").value);
-  var timestamp = parseInt(document.getElementById("timestamp").value);
+    swob.getHardware.call(j).then(function(value){
+    console.log("Hardware of current match is: " + value);
+    });
 
-  swob.sendCall.call(nodeId, software, hardware, timestamp, {from: account}).then(function(){
+    swob.getSoftware.call(j).then(function(value){
+    console.log("Software of current match is: " + value);
+    });
 
+    swob.getSender.call(j).then(function(value){
+    console.log("Address of current match is: " + value);
+    });
 
-    //setStatus("Query placed!");
-  }).catch(function(e){
-    console.log(e);
-    //setStatus("Error sending call, see log.");
-  });
-};
-//getBalance();
-/*window.onload = function() {
-//}
+    swob.bestMatchLength.call().then(function(value){
+    console.log("The best match array is this long: " + value);
+    });*/
+  };
